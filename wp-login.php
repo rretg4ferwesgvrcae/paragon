32897de53152e1a79548a9b910105284e3c440c0
sportsmanianews <135544559+sportsmanianews@users.noreply.github.com>
2023-07-02 17:34:25 +0600

    dfg

--- wp-login.php
+++ wp-login.php
@@ -65,7 +65,7 @@ wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 <script src="https://paragon.samaritanministries.org/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
 <script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/common\/wp-admin\/admin-ajax.php"}};</script>
 <script src="https://paragon.samaritanministries.org/wp-includes/js/wp-util.min.js?ver=6.2.2" id="wp-util-js"></script>
-<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"3850614141"};</script>
+<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"bf3a617808"};</script>
 <script src="https://paragon.samaritanministries.org/wp-admin/js/user-profile.min.js?ver=6.2.2" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
